3219f009734e6a3a087c3ebfa2661d7dfb8d8629
Mark Katerberg <katerberg@fastmail.fm>
2020-07-12 11:35:17 -0500

    Crashing on permissions for some reason.

--- handleListChannels.py
+++ handleListChannels.py
@@ -8,6 +8,7 @@ import logging
 import urllib.parse
 import urllib.request
 import re
+import boto3
 
 
 # Token for Slack API access
@@ -40,64 +41,29 @@ def lambda_handler(slack_event, context):
             logging.warn("Ignore bot event")
         else:
             logging.warn(slack_event)
-        #     text = slack_event["text"].strip()
-        #
-        #     if not HOOK_TOKEN == slack_event["token"]:
-        #         logging.warn("Token mismatch: " + slack_event["token"])
-        #         return "Error: security mismatch. Please contact an administrator"
-        #
-        #     # Needs to start with a user to give a high five
-        #     if not re.search("^<@", text):
-        #         return 'Error: Try `/highfive @stefanie.thelen for being Wonder Woman` instead'
-        #
-        #     # Should contain more than just a username
-        #     if re.search("^<@\w+?\|?[\w\d\._-]+>$", text):
-        #         return "Error: Try giving a reason for the high five"
-        #
-        #     # This is needed to handle a bug that emerges since mobile Slack escapes channel names even though it's not supposed to
-        #     text = text.replace("&lt;", "<")
-        #     text = text.replace("&gt;", ">")
-        #
-        #     # Get the mentioned user
-        #     textArr = text.split(">")
-        #     # Either <@U4L466|first.last> or <@U46H6654>
-        #     user_receive = textArr[0] + ">"
-        #     # Chop off the <@ from the front of the user
-        #     user_slug = user_receive[2:]
-        #     # Chop off the end of the bracket
-        #     user_slug = user_slug.split(">")[0]
-        #     # Just in case we still have the deprecated users
-        #     user_slug = user_slug.split("|")[0]
-        #
-        #     # Wrapping user_id who sent the message in escaped style
-        #     user_id = "<@" + slack_event["user_id"] + ">"
-        #
-        #     message = user_receive;
-        #     if not re.search("^ for", textArr[1]):
-        #         message = message + ": "
-        #     textArr.pop(0)
-        #
-        #     for item in textArr:
-        #         message = message + item + ">"
-        #     message = message[0:(len(message) - 1)]
-        #
-        #     values = {"text": user_id + " gave a high five to " + message}
-        #     print("message came across as " + message)
-        #
-        #     # Construct the HTTP request that will retrieve current members
-        #     gratitude_member_request = urllib.request.Request(CONVERSATION_MEMBER_URL + "?limit=" + GRATITUDE_LIMIT + "&token=" + API_TOKEN + "&channel=" + GRATITUDE_CHANNEL)
-        #     gratitude_member_json = urllib.request.urlopen(gratitude_member_request).read()
-        #     gratitude_member_list = json.loads(gratitude_member_json)["members"]
-        #
-        #     if user_slug not in gratitude_member_list:
-        #         print(user_slug + " is missing from member list")
-        #         print(gratitude_member_list)
-        #         direct_message_text = "You were mentioned in <#" + GRATITUDE_CHANNEL + ">: " + values["text"];
-        #         direct_message_request = urllib.request.Request(POST_MESSAGE_URL + "?token=" + API_TOKEN + "&channel=" + user_slug + "&text=" + urllib.parse.quote(direct_message_text))
-        #         urllib.request.urlopen(direct_message_request).read()
-        #     else:
-        #         print(user_slug + " is in member list")
-        #
+
+            s3_obj = boto3.client('s3')
+            logging.warn('set up boto3 client')
+            logging.warn(s3_obj)
+
+
+            bucketname = 'slack-private-channels-storage'
+            itemname = 'private-channels.json'
+
+            result = s3.get_bucket_policy(Bucket=bucketname)
+            logging.warn(result)
+
+            obj = s3.Object(bucketname, itemname)
+            logging.warn('got obj')
+            body = obj.get()['Body'].read()
+            logging.warn('got body')
+            logging.warn(body)
+            s3_clientobj = s3_obj.get_object(Bucket=bucketname, Key=itemname)
+            logging.warn(s3_clientobj)
+            s3_clientdata = s3_clientobj['Body'].read().decode('utf-8')
+
+            logging.warn(s3_clientdata)
+
         #     # Construct the HTTP request that will be sent to the Slack API.
 
             values = {
