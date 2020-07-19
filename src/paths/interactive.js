696303db5f61e1c427f4045a2bb27a3beb9efd79
Mark Katerberg <katerberg@fastmail.fm>
2020-07-19 18:18:56 -0500

    Reduces logging except errors and then catches one

--- src/paths/interactive.js
+++ src/paths/interactive.js
@@ -7,24 +7,6 @@ module.exports = path => server => server.post(path, interactive);
 
 const credentials = require('../../credentials.json');
 
-
-//  Example request
-// {
-//     "token": "sjdglajdg",
-//     "team_id": "TEAMID",
-//     "team_domain": "slalomstl-dev",
-//     "channel_id": "HKF786F4",
-//     "channel_name": "slack-test",
-//     "user_id": "FGJF655GG",
-//     "user_name": "first.last",
-//     "command": "/list-private",
-//     "text": "foobar",
-//     "response_url": "https://hooks.slack.com/commands/TEAMID/TRIGGER_FIRST/546gsdfakldsfg7s6gasdg",
-//     "trigger_id": "TRIGGER_FIRST.12345678.aksdgnhlusadg67sdg65sadf854356"
-// }
-
-// const {TOKEN} = credentials;
-
 function interactive(req, res) {
   try {
     const parsed = JSON.parse(req.body.payload);
@@ -64,12 +46,16 @@ This is not an excuse to be the kind of person your mother would be ashamed of.
         console.log('error from join message response'); //eslint-disable-line no-console
         console.error(e); //eslint-disable-line no-console
       });
+    }).catch(e => {
+      if (e.name === 'Error: channel_not_found') {
+        console.log('channel not found error'); //eslint-disable-line no-console
+      } else {
+        throw e;
+      }
     });
     res.send(200);
   } catch (e) {
-    console.error('BIG TIME UNHANDLED') //eslint-disable-line no-console
     console.error(e); //eslint-disable-line no-console
-    console.error(e.message); //eslint-disable-line no-console
     res.send(500, `${e}`);
   }
 }
